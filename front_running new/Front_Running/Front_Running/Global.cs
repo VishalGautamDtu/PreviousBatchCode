1f8e38942b98219426a93cab59316b17e358cd50
sanjana3011 <30316674+sanjana3011@users.noreply.github.com>
2018-09-11 16:21:38 +0530

    Add files via upload
    FRONT RUNNING NEW

---
+++ front_running new/Front_Running/Front_Running/Global.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Front_Running
+{
+    public static class Global
+    {
+        public static string uname;
+        public static int id=1;
+
+    }
+}
